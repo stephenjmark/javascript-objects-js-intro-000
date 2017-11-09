d4b5c93ce119516ec24203703b2fd11bd5cb4aa0
Stephen Mark <stephenjmark@gmail.com>
2017-11-09 06:42:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist[artist];
+  delete playlist[artistName];
 
   return playlist
 }
